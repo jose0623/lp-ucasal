06a1e0a655faad5f5d18c39b2ae789d3caeeeba8
Jose Brito <josebrito0623@gmail.com>
2018-12-15 21:14:14 -0400

    Update Slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -54,6 +54,33 @@ var swiper2 = new Swiper('.swiper-container-testimony', {
     },
   });
 
+
+
+  $(document).ready(function() {
+    new Swiper('.swiper-container-banner', {
+      speed: 400,
+      spaceBetween: 30,
+      disableOnInteraction: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+      },
+      pagination: {
+        el: '.swiper-pagination-banner',
+        clickable: true,
+      }
+    });
+    var mySwiper = document.querySelector('.swiper-container-banner').swiper
+
+    $(".swiper-container-banner").mouseenter(function() {
+      mySwiper.autoplay.stop();
+    });
+
+    $(".swiper-container-banner").mouseleave(function() {
+      mySwiper.autoplay.start();
+    });
+  });
+
   $('#nombre').keypress(function(tecla) {
     if((tecla.charCode < 97 || tecla.charCode > 122) && (tecla.charCode < 65 || tecla.charCode > 90) && (tecla.charCode != 45) && (tecla.charCode != 209) && (tecla.charCode != 241) && (tecla.charCode != 32)) return false;
   });
